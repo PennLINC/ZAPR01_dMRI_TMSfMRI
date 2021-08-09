cb66184ae884cbfed723e5f8e4491aa5b492e6f9
vsydnor <valeriejill.sydnor@gmail.com>
2021-08-09 14:29:14 -0400

    brodmann endpoints FOD template

--- tract_analyses/anatomy/tractorigins_brodmann.sh
+++ tract_analyses/anatomy/tractorigins_brodmann.sh
@@ -6,4 +6,7 @@ ZAP=/storage/vsydnor/ZAPR01_WhiteMatter_TMSfMRI
 
 3dresample -master /usr/local/fsl-5.0.11/data/standard/MNI152_T1_2mm_brain.nii.gz -inset $ZAP/templates/Brodmann/Brodmann_ICBM152.nii.gz -prefix $ZAP/templates/Brodmann/Brodmann_ICBM152-MNI-2mm.nii.gz 
 
-3dROIstats -mask $ZAP/templates/Brodmann/Brodmann_ICBM152-MNI-2mm.nii.gz -nzvoxels $ZAP/mrtrix_fixelanalysis/siteofstim_subcortical_tracts/voxels/AmygdalaSOS-LHAmygdalaROI25-tracts-endpoints-vlPFC-inMNIspace.nii.gz  >> $ZAP/output_measures/tract_endpoints_brodmann/Brodmann_areas.csv
+antsApplyTransforms -d 3 -i $ZAP/templates/Brodmann/Brodmann_ICBM152-MNI-2mm.nii.gz -r $ZAP/mrtrix_fixelanalysis/population_template/ZAPR01_FOD_Template_l0.nii.gz -o $ZAP/templates/Brodmann/Brodmann_ICBM152-inFODTemplate.nii.gz -n NearestNeighbor -t $ZAP/templates/FSL_HCP1065_FA_1.3mm-inFODTemplate1Warp.nii.gz -t $ZAP/templates/FSL_HCP1065_FA_1.3mm-inFODTemplate0GenericAffine.mat
+
+3dROIstats -mask $ZAP/templates/Brodmann/Brodmann_ICBM152-inFODTemplate.nii.gz -nzvoxels $ZAP/mrtrix_fixelanalysis/siteofstim_subcortical_tracts/voxels/AmygdalaSOS-LHAmygdalaROI25-tracts-endpoints-vlPFC.nii.gz >>  $ZAP/output_measures/tract_endpoints_brodmann/Brodmann_areas.csv
+
