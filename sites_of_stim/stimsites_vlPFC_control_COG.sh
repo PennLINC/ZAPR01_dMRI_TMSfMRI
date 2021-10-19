c5d74b57f4fcfdc623f793c55210f348683d4e81
vsydnor <valeriejill.sydnor@gmail.com>
2021-10-18 20:09:20 -0400

    intensity-weighted stim sites COG

--- sites_of_stim/stimsites_vlPFC_control_COG.sh
+++ sites_of_stim/stimsites_vlPFC_control_COG.sh
@@ -5,13 +5,13 @@
 ZAP=/storage/vsydnor/ZAPR01_WhiteMatter_TMSfMRI
 
 #vlPFC TMS sites center of gravity
-#fslstats Amygdala-SitesofStimulation-StudyMask-MNI-2mm.nii.gz  -C : 68.811006 80.116279 43.196856 
-fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 68.811006 1 80.116279 1 43.196856 1 0 1 $ZAP/sites_of_stim/Amygdala-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz
+#fslstats Amygdala-SitesofStimulation-StudyMask-weighted-MNI-2mm.nii.gz  -C : 69.600000 79.888889 42.177778  
+fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 69.600000 1 79.888889 1 42.177778 1 0 1 $ZAP/sites_of_stim/Amygdala-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz 
  
 fslmaths $ZAP/sites_of_stim/Amygdala-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz -dilM -kernel sphere 6.5 -fmean -bin $ZAP/sites_of_stim/Amygdala-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz
 
 #control TMS sites center of gravity
-#fslstats sgACC-SitesofStimulation-StudyMask-MNI-2mm.nii.gz -C : 61.747186 80.606936 55.230910 
-fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 61.747186 1 80.606936 1 55.230910 1 0 1 $ZAP/sites_of_stim/sgACC-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz 
+#fslstats sgACC-SitesofStimulation-StudyMask-weighted-MNI-2mm.nii.gz -C : 61.111111 80.066667 56.977778 
+fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 61.111111 1 80.066667 1 56.977778 1 0 1 $ZAP/sites_of_stim/sgACC-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz 
 
 fslmaths $ZAP/sites_of_stim/sgACC-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz -dilM -kernel sphere 6.5 -fmean -bin $ZAP/sites_of_stim/sgACC-SitesofStimulation-GroupCOG-MNI-2mm.nii.gz
